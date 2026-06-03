cbd46e912970d25f7baf4dee61a610f9cc9fba26
SyaOtiLan <954239196@qq.com>
2026-06-03 12:25:06 +0800

    Force device-width WebView layout

--- android/ContextOSWebView/app/src/main/java/com/contextos/webview/MainActivity.java
+++ android/ContextOSWebView/app/src/main/java/com/contextos/webview/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -21,6 +22,9 @@ public class MainActivity extends Activity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         webView = new WebView(this);
+        webView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
@@ -29,17 +33,33 @@ public class MainActivity extends Activity {
         settings.setDatabaseEnabled(true);
         settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        settings.setUseWideViewPort(true);
+        settings.setUseWideViewPort(false);
         settings.setLoadWithOverviewMode(false);
         settings.setSupportZoom(false);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
         settings.setTextZoom(100);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setInitialScale(100);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.evaluateJavascript(
+                        "(() => {"
+                                + "let m=document.querySelector('meta[name=viewport]');"
+                                + "if(!m){m=document.createElement('meta');m.name='viewport';document.head.appendChild(m);}"
+                                + "m.content='width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover';"
+                                + "document.documentElement.style.width='100%';"
+                                + "document.body.style.width='100%';"
+                                + "document.body.style.overflowX='hidden';"
+                                + "})()",
+                        null);
+            }
+        });
         webView.setWebChromeClient(new WebChromeClient());
         webView.clearCache(true);
-        webView.loadUrl("http://47.108.145.21:5000/ui/code?app=android&v=20260603");
+        webView.loadUrl("http://47.108.145.21:5000/ui/code?app=android&v=20260603b");
     }
 
     @Override
